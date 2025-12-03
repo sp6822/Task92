1da7f4939bc5571fc2da0f9aea2608f3567aae46
sp6822 <sp6822@bs.amalne.k12.il>
2025-12-04 00:17:25 +0200

    task9

--- app/src/main/java/com/example/task9/MainActivity.java
+++ app/src/main/java/com/example/task9/MainActivity.java
@@ -1,15 +1,13 @@
+
 package com.example.task9;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
         String s1 = num1.getText().toString();
         String s2 = num2.getText().toString();
@@ -42,3 +44,34 @@ public class MainActivity extends AppCompatActivity {
             answer.setText("");
             return true;
         }
+
+        if (s1.isEmpty() || s2.isEmpty()) {
+            answer.setText("יש להזין שני מספרים");
+            return true;
+        }
+
+        double a = Double.parseDouble(s1);
+        double b = Double.parseDouble(s2);
+        double result = 0;
+
+        if (id == R.id.add) {
+            result = a + b;
+        }
+        else if (id == R.id.sub) {
+            result = a - b;
+        }
+        else if (id == R.id.mul) {
+            result = a * b;
+        }
+        else if (id == R.id.div) {
+            if (b == 0) {
+                answer.setText("אי אפשר לחלק באפס");
+                return true;
+            }
+            result = a / b;
+        }
+
+        answer.setText(String.valueOf(result));
+        return true;
+    }
+}
